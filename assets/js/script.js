f16621db5a36b0222c65d6483e3888b498f21007
Juan Sebastian <juan_seb07@hotmail.com>
2022-08-01 00:36:23 -0500

    Se crea funcion listener para capturar y agregar nueva tarea, adicionalmente se modifica la estructura html del container.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 const btn = document.querySelector("[data-form-btn]");
 
+
+btn.addEventListener("click", (e) => {
+  const input = document.querySelector("[data-form-input]");
+  const task = document.createElement("li");
+  const list = document.querySelector("[data-list]");
+  e.preventDefault();
+  task.classList.add("container__task");
+  let texto = input.value;
+  input.value = "";
+  const content = `
+    <i class="fas fa-check-circle task__check--icon"></i>
+    <div class="task__description">
+      ${texto}
+    </div>
+    <i class="fas fa-trash task__trash--icon"></i>
+  `;
+  task.innerHTML = content;
+  list.appendChild(task)
+});
